2824537dc00b2c6690e4a8b8e4f4925e3d7bc206
jerryhuang3 <jerryhuang@utexas.edu>
2019-03-13 14:00:08 +0000

    created global users object to store user information

--- express_server.js
+++ express_server.js
@@ -10,12 +10,23 @@ app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
-var urlDatabase = {
+const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
 
-
+const users = {
+    "userRandomID": {
+        id: "userRandomID",
+        email: "user@example.com",
+        password: "purple-monkey-dinosaur"
+    },
+    "user2RandomID": {
+        id: "user2RandomID",
+        email: "user2@example.com",
+        password: "dishwasher-funk"
+    }
+}
 
 
 app.get("/", (req, res) => {
