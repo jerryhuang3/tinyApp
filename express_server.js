292c683c0c8794042b33bf73a6bc52cecfa2c975
jerryhuang3 <jerryhuang@utexas.edu>
2019-03-14 19:01:24 +0000

    port

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
-var PORT = (process.env.PORT || 3000);
+const port = process.env.PORT || 8000;
 app.set("view engine", "ejs");
+let server = require('http').Server(app);
 
 const bcrypt = require('bcrypt');
 
@@ -104,8 +105,8 @@ app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
 });
 
 app.post("/urls/", (req, res) => {
